5af7c80a08cb5f20f9e772d1f620d5abba8bb30b
Corvus Rex <adrianovriadi@gmail.com>
2026-05-24 23:52:07 +0700

    Vercel deployment fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,9 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
